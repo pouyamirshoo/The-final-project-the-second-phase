6b2d78c984d27fa6cdda03a3540b2a7229834bb6
pc <poyatajhiz59@gmail.com>
2024-06-08 15:16:07 -0800

    creat OfferRepository interface

---
+++ src/main/java/com/example/finalprojectsecondphase/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectsecondphase.repository;
+
+import com.example.finalprojectsecondphase.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer,Integer> {
+}
