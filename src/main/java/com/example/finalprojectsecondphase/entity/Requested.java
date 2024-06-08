12484301ed02b60aeaf41e3c425342b7303e8e71
pc <poyatajhiz59@gmail.com>
2024-06-08 15:18:21 -0800

    fill Requested entity class

--- src/main/java/com/example/finalprojectsecondphase/entity/Requested.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/Requested.java
@@ -1,4 +1,28 @@
 package com.example.finalprojectsecondphase.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@SoftDelete
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class Requested {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @OneToOne
+    Expert expert;
+    @ToString.Exclude
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    List<SubDuty> subDuties;
 }
