72025a2211488fefdf6c70cf02f184ab4c6e28e9
pc <poyatajhiz59@gmail.com>
2024-06-08 15:26:30 -0800

    creat FinalProjectSecondPhaseApplicationTests class

---
+++ src/test/java/com/example/finalprojectsecondphase/FinalProjectSecondPhaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectsecondphase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSecondPhaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
