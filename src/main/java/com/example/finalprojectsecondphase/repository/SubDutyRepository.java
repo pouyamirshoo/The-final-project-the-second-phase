8094a14eba47c6bd6193d91e9560b190b7ae9d0d
pc <poyatajhiz59@gmail.com>
2024-06-08 15:15:28 -0800

    creat SubDutyRepository interface

---
+++ src/main/java/com/example/finalprojectsecondphase/repository/SubDutyRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectsecondphase.repository;
+
+import com.example.finalprojectsecondphase.entity.SubDuty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubDutyRepository extends JpaRepository<SubDuty,Integer> {
+}
