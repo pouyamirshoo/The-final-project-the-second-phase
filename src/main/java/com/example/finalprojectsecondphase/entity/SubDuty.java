b0002271c718c526f98527f11e204f35616f3a92
pc <poyatajhiz59@gmail.com>
2024-06-08 15:18:11 -0800

    fill SubDuty entity class

--- src/main/java/com/example/finalprojectsecondphase/entity/SubDuty.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/SubDuty.java
@@ -1,4 +1,40 @@
 package com.example.finalprojectsecondphase.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@SoftDelete
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class SubDuty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(unique = true,name = "sub_duty_name")
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "sub duty name can not be null")
+    String subDutyName;
+    @NotNull(message = "sub duty price can not be null")
+    Integer price;
+    @Column(columnDefinition = "TEXT")
+    @NotNull(message = "description most not null")
+    String description;
+    @ToString.Exclude
+    @ManyToOne
+    Duty duty;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "subDuty",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
+    List<Order> orders;
 }
