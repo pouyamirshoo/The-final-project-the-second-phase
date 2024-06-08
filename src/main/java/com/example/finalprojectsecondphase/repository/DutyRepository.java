7fa0d0276bf82aaecd28532c84b445908c2af043
pc <poyatajhiz59@gmail.com>
2024-06-08 15:16:24 -0800

    creat DutyRepository interface

---
+++ src/main/java/com/example/finalprojectsecondphase/repository/DutyRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectsecondphase.repository;
+
+import com.example.finalprojectsecondphase.entity.Duty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DutyRepository extends JpaRepository<Duty,Integer> {
+}
