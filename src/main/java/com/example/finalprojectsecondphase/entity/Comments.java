90cfc93f353f0cd3d96adf083ebf156c0f9ace3d
pc <poyatajhiz59@gmail.com>
2024-06-08 15:20:34 -0800

    fill Comments entity class

--- src/main/java/com/example/finalprojectsecondphase/entity/Comments.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/Comments.java
@@ -1,4 +1,32 @@
 package com.example.finalprojectsecondphase.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SoftDelete;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@SoftDelete
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @OneToOne
+    Order order;
+    @NotNull
+    @Min(0)
+    @Max(5)
+    int rate;
+    @Column(name = "additional_comments",columnDefinition = "TEXT")
+    String additionalComments;
 }
