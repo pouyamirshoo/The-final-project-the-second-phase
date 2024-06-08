d1dba2f17a794d734bbfaefc49672ac794295dda
pc <poyatajhiz59@gmail.com>
2024-06-08 15:19:22 -0800

    fill Duty entity class

--- src/main/java/com/example/finalprojectsecondphase/entity/Duty.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/Duty.java
@@ -1,4 +1,32 @@
 package com.example.finalprojectsecondphase.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@SoftDelete
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class Duty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(name = "duty_name")
+    @Pattern(regexp = "[a-zA-Z]+")
+    @NotNull(message = "duty name can not be null")
+    String dutyName;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "duty",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    List<SubDuty> subDuties;
 }
