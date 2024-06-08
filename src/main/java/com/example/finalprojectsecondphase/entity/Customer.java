809be17bf8ff661a692db47d288c2b069df43771
pc <poyatajhiz59@gmail.com>
2024-06-08 15:20:20 -0800

    fill Customer entity class

--- src/main/java/com/example/finalprojectsecondphase/entity/Customer.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/Customer.java
@@ -1,4 +1,32 @@
 package com.example.finalprojectsecondphase.entity;
 
-public class Customer {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SoftDelete;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@SoftDelete
+@SuperBuilder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Customer extends Person{
+    @Column(name = "customer_balance")
+    Integer customerBalance;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    List<Order> orders;
+
+    @PrePersist
+    public void defaultValues() {
+        if (customerBalance == null)
+            customerBalance = 0;
+    }
 }
