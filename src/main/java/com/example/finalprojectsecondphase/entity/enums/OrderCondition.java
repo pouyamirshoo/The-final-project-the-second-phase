d61e22f65a2db8d5226718fbe0bd49a4fd7c8b27
pc <poyatajhiz59@gmail.com>
2024-06-08 15:11:58 -0800

    creat OrderCondition enum class

---
+++ src/main/java/com/example/finalprojectsecondphase/entity/enums/OrderCondition.java
@@ -0,0 +1,10 @@
+package com.example.finalprojectsecondphase.entity.enums;
+
+public enum OrderCondition {
+    RECEIVING_OFFERS,
+    WAIT_FOR_ACCEPT,
+    ACCEPTED,
+    ONGOING,
+    DONE,
+    PAID
+}
