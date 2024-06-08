73426591d08b4930d5dc233636e28aa16c7aa85a
pc <poyatajhiz59@gmail.com>
2024-06-08 15:23:36 -0800

    creat FinalProjectSecondPhaseApplication in Spring

---
+++ src/main/java/com/example/finalprojectsecondphase/FinalProjectSecondPhaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectsecondphase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSecondPhaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSecondPhaseApplication.class, args);
+    }
+
+}
