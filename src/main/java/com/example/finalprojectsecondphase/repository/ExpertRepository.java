081c99cd167bd92a29be29dbc75e5abc6d00d54a
pc <poyatajhiz59@gmail.com>
2024-06-08 15:16:16 -0800

    creat ExpertRepository interface

---
+++ src/main/java/com/example/finalprojectsecondphase/repository/ExpertRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectsecondphase.repository;
+
+import com.example.finalprojectsecondphase.entity.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpertRepository extends JpaRepository<Expert,Integer> {
+}
