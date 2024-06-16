48b13a9dccb32fb39a6a6cacba68d930483fdc6a
pc <poyatajhiz59@gmail.com>
2024-06-16 00:00:51 -0800

    creat WrongImageInputException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/WrongImageInputException.java
@@ -0,0 +1,5 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class WrongImageInputException extends RuntimeException{
+    public WrongImageInputException(String message){super(message);}
+}
