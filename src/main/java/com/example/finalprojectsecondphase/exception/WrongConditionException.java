d3cd341ef8d63db1ffc9cef9401ba3b6439d8cf9
pc <poyatajhiz59@gmail.com>
2024-06-16 00:01:23 -0800

    creat WrongConditionException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/WrongConditionException.java
@@ -0,0 +1,5 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class WrongConditionException extends RuntimeException {
+    public WrongConditionException(String message){super(message);}
+}
