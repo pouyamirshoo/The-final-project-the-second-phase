da1dedc71b2828334ec251667a29a2903548622f
pc <poyatajhiz59@gmail.com>
2024-06-16 00:01:03 -0800

    creat WrongDateInsertException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/WrongDateInsertException.java
@@ -0,0 +1,5 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class WrongDateInsertException extends RuntimeException{
+    public WrongDateInsertException(String message){super(message);}
+}
