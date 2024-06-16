bada6cb8191fbb41ea91701f4de8039f8ce205dc
pc <poyatajhiz59@gmail.com>
2024-06-16 00:00:42 -0800

    creat WrongInputPriceException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/WrongInputPriceException.java
@@ -0,0 +1,5 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class WrongInputPriceException extends RuntimeException{
+    public WrongInputPriceException (String message){super(message);}
+}
