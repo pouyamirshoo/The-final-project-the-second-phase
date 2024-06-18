6edfb6ba3716394b27cd0eda110c3861d6a7f7e7
pc <poyatajhiz59@gmail.com>
2024-06-17 16:56:01 -0800

    add findByPhoneNumber CustomerRepository interface

--- src/main/java/com/example/finalprojectsecondphase/repository/CustomerRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/CustomerRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Optional<Customer> findByUsername(String username);
-
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
 }
