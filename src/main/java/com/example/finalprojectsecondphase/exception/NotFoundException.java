8e47753a42145b3598c1127935cae7163cc6f2fa
pc <poyatajhiz59@gmail.com>
2024-06-17 17:03:08 -0800

    creat NotFoundException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String massage) {
+        super(massage);
+    }
+}
