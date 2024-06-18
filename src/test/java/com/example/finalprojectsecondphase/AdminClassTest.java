6a970a8bc48770a06993b38f750e7db4f2a5e214
pc <poyatajhiz59@gmail.com>
2024-06-17 18:52:32 -0800

    add static makeAdmin in AdminClassTest

--- src/test/java/com/example/finalprojectsecondphase/AdminClassTest.java
+++ src/test/java/com/example/finalprojectsecondphase/AdminClassTest.java
@@ -17,4 +17,29 @@ import java.util.List;
 @Slf4j
 public class AdminClassTest {
 
+
+    private static Admin admin;
+
+    private static SubDuty thirdCorrectSubDuty;
+
+    @BeforeAll
+    public static void makeAdmin() {
+        admin = Admin.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya123")
+                .password("aaAA12!@")
+                .email("pouya@gmail.com")
+                .phoneNumber("+989124184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1395613373")
+                .build();
+
+        thirdCorrectSubDuty = SubDuty.builder()
+                .subDutyName("daftar")
+                .price(110000)
+                .description("this price is for an hour")
+                .build();
+    }
 }
