de78183e7d739b4eee38d0898c4a4b730c2889f3
pc <poyatajhiz59@gmail.com>
2024-06-17 19:19:38 -0800

    creat CustomerClassTest class

---
+++ src/test/java/com/example/finalprojectsecondphase/CustomerClassTest.java
@@ -0,0 +1,18 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.entity.*;
+import com.example.finalprojectsecondphase.exception.*;
+import com.example.finalprojectsecondphase.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+class CustomerClassTest {
+
+}
