453592d265c55b83c7f13ccf662652532394c07d
pc <poyatajhiz59@gmail.com>
2024-06-17 17:13:56 -0800

    add findByExpert RequestRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/RequestRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/RequestRepository.java
@@ -1,7 +1,11 @@
 package com.example.finalprojectsecondphase.repository;
 
-import com.example.finalprojectsecondphase.entity.Requested;
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RequestRepository extends JpaRepository<Requested,Integer> {
+import java.util.Optional;
+
+public interface RequestRepository extends JpaRepository<Request,Integer> {
+    Optional<Request> findByExpert(Expert expert);
 }
