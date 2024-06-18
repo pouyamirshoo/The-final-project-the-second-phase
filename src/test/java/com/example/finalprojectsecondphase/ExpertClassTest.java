9de8b652ffe06295eb4a7b18fc37ac7b12078f57
pc <poyatajhiz59@gmail.com>
2024-06-17 19:41:55 -0800

    add static makeExpert in ExpertClassTest

--- src/test/java/com/example/finalprojectsecondphase/ExpertClassTest.java
+++ src/test/java/com/example/finalprojectsecondphase/ExpertClassTest.java
@@ -20,4 +20,122 @@ import java.util.List;
 @Slf4j
 public class ExpertClassTest {
 
+    String correctPath = "F:\\Maktab\\FinalProjectFirstPhase\\src\\main\\java\\images\\15639454.jpg";
+
+    private static Expert correctExpert;
+    private static Expert deleteExpert;
+    private static Expert duplicateUsernameExpert;
+    private static Expert duplicateEmailExpert;
+    private static Expert duplicatePhoneNumberExpert;
+    private static Expert duplicatePostalCodeExpert;
+    private static Expert duplicateNationalCodeExpert;
+    private static Expert invalidInfoTypeExpert;
+
+    @BeforeAll
+    public static void makeExpert() {
+
+        correctExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya123")
+                .password("aaAA12!@")
+                .email("pouya@gmail.com")
+                .phoneNumber("+989124184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1395613373")
+                .nationalCode("0440186412")
+                .build();
+
+        deleteExpert = Expert.builder()
+                .firstname("mohammd")
+                .lastname("mir")
+                .username("moh123")
+                .password("aaAA12!@")
+                .email("moh@gmail.com")
+                .phoneNumber("+989128544434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1665743471")
+                .nationalCode("5126973215")
+                .build();
+
+        duplicateUsernameExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya123")
+                .password("aaAA12!@")
+                .email("pouyaaa@gmail.com")
+                .phoneNumber("+989125184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1417635471")
+                .nationalCode("2509203669")
+                .build();
+
+        duplicateEmailExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1234")
+                .password("aaAA12!@")
+                .email("pouya@gmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("4654190118")
+                .nationalCode("7829168131")
+                .build();
+
+        duplicatePhoneNumberExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1235")
+                .password("aaAA12!@")
+                .email("pouyaaa@gmail.com")
+                .phoneNumber("+989124184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1665743471")
+                .nationalCode("8583552363")
+                .build();
+
+        duplicatePostalCodeExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1236")
+                .password("aaAA12!@")
+                .email("pouysaa@gmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1395613373")
+                .nationalCode("1964576288")
+                .build();
+
+        duplicateNationalCodeExpert = Expert.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1239")
+                .password("aaAA12!@")
+                .email("pouyyyya@gmail.com")
+                .phoneNumber("+989124294434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("3336131927")
+                .nationalCode("0440186412")
+                .build();
+
+        invalidInfoTypeExpert = Expert.builder()
+                .firstname("pouya@#")
+                .lastname("mir")
+                .username("pouya1236")
+                .password("aaA1!@")
+                .email("pouysaadfagmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("135613373")
+                .nationalCode("15464818971")
+                .build();
+    }
 }
