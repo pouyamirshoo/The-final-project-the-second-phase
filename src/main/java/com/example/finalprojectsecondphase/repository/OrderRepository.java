df63448e793d5930e2e911ee013f72a164e91683
pc <poyatajhiz59@gmail.com>
2024-06-17 17:12:20 -0800

    add findByCustomer OrderRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/OrderRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.example.finalprojectsecondphase.repository;
 
+import com.example.finalprojectsecondphase.entity.Customer;
 import com.example.finalprojectsecondphase.entity.Order;
+import com.example.finalprojectsecondphase.entity.SubDuty;
+import com.example.finalprojectsecondphase.entity.enums.OrderCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+    List<Order> findByCustomer(Customer customer);
 }
