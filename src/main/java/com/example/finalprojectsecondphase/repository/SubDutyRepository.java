580a6cefd497044f6821cbe8fc9e6199c94023eb
pc <poyatajhiz59@gmail.com>
2024-06-17 17:14:34 -0800

    add findBySubDutyName SubDutyRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/SubDutyRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/SubDutyRepository.java
@@ -1,7 +1,12 @@
 package com.example.finalprojectsecondphase.repository;
 
+import com.example.finalprojectsecondphase.entity.Duty;
 import com.example.finalprojectsecondphase.entity.SubDuty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SubDutyRepository extends JpaRepository<SubDuty,Integer> {
+    Optional<SubDuty> findBySubDutyName(String name);
 }
