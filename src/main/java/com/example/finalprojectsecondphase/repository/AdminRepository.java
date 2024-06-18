26cbac508fd9aead8b0fe1678619445ea5c8c7f8
pc <poyatajhiz59@gmail.com>
2024-06-17 16:50:04 -0800

    creat AdminRepository interface

---
+++ src/main/java/com/example/finalprojectsecondphase/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.example.finalprojectsecondphase.repository;
+
+import com.example.finalprojectsecondphase.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin,Integer> {
+
+}
