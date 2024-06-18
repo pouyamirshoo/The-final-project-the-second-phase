9cd5516288aafc6cf5d7cc30e14166d4ff581a4e
pc <poyatajhiz59@gmail.com>
2024-06-17 19:05:16 -0800

    creat CreatAndValidationDateClassTest class

---
+++ src/test/java/com/example/finalprojectsecondphase/CreatAndValidationDateClassTest.java
@@ -0,0 +1,18 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.exception.InvalidInputInformationException;
+import com.example.finalprojectsecondphase.exception.WrongDateInsertException;
+import com.example.finalprojectsecondphase.util.validation.CreatAndValidationDate;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+public class CreatAndValidationDateClassTest {
+
+}
