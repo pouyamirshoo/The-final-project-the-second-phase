f5ab66ae6d55ed809917bff856c3b5ef24c85a58
pc <poyatajhiz59@gmail.com>
2024-06-17 17:19:57 -0800

    remove ToString

--- src/main/java/com/example/finalprojectsecondphase/entity/Expert.java
+++ src/main/java/com/example/finalprojectsecondphase/entity/Expert.java
@@ -1,7 +1,6 @@
 package com.example.finalprojectsecondphase.entity;
 
 import com.example.finalprojectsecondphase.entity.enums.ExpertCondition;
-import com.example.finalprojectsecondphase.entity.enums.UpdatedField;
 import com.example.finalprojectsecondphase.util.validation.ValidationCode;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
@@ -17,45 +16,39 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-@ToString(callSuper = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @SoftDelete
 @Entity
-public class Expert  extends Person{
-    @Column(unique = true,name = "national_code")
+public class Expert extends Person {
+    @Column(unique = true, name = "national_code")
     @NotNull(message = "Expert national code can not be null")
     @ValidationCode
     String nationalCode;
-    @ToString.Exclude
     @Column(name = "expert_image")
     @Lob
-    @Size(max = 300000,message = "it is too big file")
+    @Size(max = 300000, message = "it is too big file for image")
     @NotEmpty(message = "must upload an image")
     byte[] expertImage;
     @Column(name = "expert_condition")
     @Enumerated(EnumType.STRING)
     ExpertCondition expertCondition;
-    @Column(columnDefinition = "TEXT",name = "reject_reason")
+    @Column(columnDefinition = "TEXT", name = "reject_reason")
     String rejectReason;
     @Max(5)
     Integer rate;
     @Min(0)
     Integer balance;
-    @Enumerated(EnumType.STRING)
-    UpdatedField updatedField;
-    @ToString.Exclude
-    @ManyToMany(cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     List<SubDuty> subDuties;
-    @ToString.Exclude
-    @OneToMany(mappedBy = "expert",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "expert", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     List<Offer> offers;
 
     @PrePersist
-    public void defaultValues(){
-        if (expertCondition == null){
+    public void defaultValues() {
+        if (expertCondition == null) {
             expertCondition = ExpertCondition.AWAITING;
         }
-        if (balance == null){
+        if (balance == null) {
             balance = 0;
         }
         if (rate == null) {
