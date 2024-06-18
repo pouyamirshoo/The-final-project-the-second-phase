0a0171e1d209f9ff0dd3a325c60ecb3d00149418
pc <poyatajhiz59@gmail.com>
2024-06-17 18:48:02 -0800

    creat AdminClassTest @SpringBootTest

---
+++ src/test/java/com/example/finalprojectsecondphase/AdminClassTest.java
@@ -0,0 +1,20 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.entity.*;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
+import com.example.finalprojectsecondphase.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+public class AdminClassTest {
+
+}
