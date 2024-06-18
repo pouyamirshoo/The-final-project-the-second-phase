3b7eb19059dcf3db07741725cba213897e741a30
pc <poyatajhiz59@gmail.com>
2024-06-17 21:59:46 -0800

    creat TakeAndCheckImageClassTest class

---
+++ src/test/java/com/example/finalprojectsecondphase/TakeAndCheckImageClassTest.java
@@ -0,0 +1,21 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.exception.WrongImageInputException;
+import com.example.finalprojectsecondphase.service.ExpertService;
+import com.example.finalprojectsecondphase.util.validation.TakeAndCheckImage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+public class TakeAndCheckImageClassTest {
+
+}
