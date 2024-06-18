b77c483393a4fcdb4e1f2c57ce77d3a7cd360021
pc <poyatajhiz59@gmail.com>
2024-06-17 18:36:26 -0800

    add checkOfferPrice in OfferService

--- src/main/java/com/example/finalprojectsecondphase/service/OfferService.java
+++ src/main/java/com/example/finalprojectsecondphase/service/OfferService.java
@@ -1,11 +1,39 @@
 package com.example.finalprojectsecondphase.service;
 
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.Offer;
+import com.example.finalprojectsecondphase.entity.Order;
+import com.example.finalprojectsecondphase.entity.SubDuty;
+import com.example.finalprojectsecondphase.entity.enums.OfferCondition;
+import com.example.finalprojectsecondphase.entity.enums.OrderCondition;
+import com.example.finalprojectsecondphase.exception.DuplicateInformationException;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
+import com.example.finalprojectsecondphase.exception.WrongConditionException;
+import com.example.finalprojectsecondphase.exception.WrongInputPriceException;
 import com.example.finalprojectsecondphase.repository.OfferRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OfferService {
-    private final OfferRepository offerRepository;
+
+
+
+    public boolean checkOfferPrice(int price, SubDuty subDuty) {
+        int subDutyPrice = subDuty.getPrice();
+        if (price >= subDutyPrice)
+            return true;
+        else throw new WrongInputPriceException("offer price can not be less than default price");
+    }
 }
