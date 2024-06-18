b8bf787468e95574ff40aaae69df6a626d728f57
pc <poyatajhiz59@gmail.com>
2024-06-17 17:02:58 -0800

    creat InvalidInputInformationException class

---
+++ src/main/java/com/example/finalprojectsecondphase/exception/InvalidInputInformationException.java
@@ -0,0 +1,5 @@
+package com.example.finalprojectsecondphase.exception;
+
+public class InvalidInputInformationException extends RuntimeException{
+    public InvalidInputInformationException(String message){super(message);}
+}
