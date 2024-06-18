5f52b7626bde6e46ac10409763c76ed4337a4f81
pc <poyatajhiz59@gmail.com>
2024-06-17 17:16:33 -0800

    Sorted

--- src/main/java/com/example/finalprojectsecondphase/repository/CommentsRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/CommentsRepository.java
@@ -3,5 +3,5 @@ package com.example.finalprojectsecondphase.repository;
 import com.example.finalprojectsecondphase.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentsRepository extends JpaRepository<Comments,Integer> {
+public interface CommentsRepository extends JpaRepository<Comments, Integer> {
 }
