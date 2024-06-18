c87f10edcff75cba3b3b7412e22902999d1d48f0
pc <poyatajhiz59@gmail.com>
2024-06-17 19:00:41 -0800

    creat CommentsClassTest @SpringBootTest

---
+++ src/test/java/com/example/finalprojectsecondphase/CommentsClassTest.java
@@ -0,0 +1,19 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.entity.Comments;
+import com.example.finalprojectsecondphase.entity.Order;
+import com.example.finalprojectsecondphase.exception.InvalidInputInformationException;
+import com.example.finalprojectsecondphase.service.CommentsService;
+import com.example.finalprojectsecondphase.service.OrderService;
+import com.example.finalprojectsecondphase.service.SubDutyService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+public class CommentsClassTest {
+
+}
