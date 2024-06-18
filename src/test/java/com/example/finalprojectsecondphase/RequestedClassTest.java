43e626fb49e1f4b4d2dd86eac9439072dc2ac77f
pc <poyatajhiz59@gmail.com>
2024-06-17 21:48:16 -0800

    creat RequestedClassTest class

---
+++ src/test/java/com/example/finalprojectsecondphase/RequestedClassTest.java
@@ -0,0 +1,23 @@
+package com.example.finalprojectsecondphase;
+
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.Request;
+import com.example.finalprojectsecondphase.entity.SubDuty;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
+import com.example.finalprojectsecondphase.service.ExpertService;
+import com.example.finalprojectsecondphase.service.RequestService;
+import com.example.finalprojectsecondphase.service.SubDutyService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+public class RequestedClassTest {
+
+}
