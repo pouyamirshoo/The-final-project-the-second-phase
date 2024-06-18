2b2426aeaa5b66baa02dd1b2e03bb32603aa8138
pc <poyatajhiz59@gmail.com>
2024-06-17 17:03:45 -0800

    add findByDutyName DutyRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/DutyRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/DutyRepository.java
@@ -3,5 +3,8 @@ package com.example.finalprojectsecondphase.repository;
 import com.example.finalprojectsecondphase.entity.Duty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DutyRepository extends JpaRepository<Duty,Integer> {
+    Optional<Duty> findByDutyName(String name);
 }
