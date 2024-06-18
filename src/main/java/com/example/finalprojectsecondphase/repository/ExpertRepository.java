d8b5b1921630d7aa404401cbe4980c608b67eb02
pc <poyatajhiz59@gmail.com>
2024-06-17 17:05:01 -0800

    add findByUsername ExpertRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/ExpertRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/ExpertRepository.java
@@ -1,7 +1,13 @@
 package com.example.finalprojectsecondphase.repository;
 
 import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.enums.ExpertCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ExpertRepository extends JpaRepository<Expert,Integer> {
+    Optional<Expert> findByUsername(String username);
+
 }
