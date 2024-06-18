c8f9893c401006cb54d4dc2fcc2e9dbe648ac81b
pc <poyatajhiz59@gmail.com>
2024-06-17 17:40:44 -0800

    add validate and saveCustomer in CustomerService

--- src/main/java/com/example/finalprojectsecondphase/service/CustomerService.java
+++ src/main/java/com/example/finalprojectsecondphase/service/CustomerService.java
@@ -1,11 +1,57 @@
 package com.example.finalprojectsecondphase.service;
 
+import com.example.finalprojectsecondphase.entity.Customer;
+import com.example.finalprojectsecondphase.exception.DuplicateInformationException;
+import com.example.finalprojectsecondphase.exception.InvalidInputInformationException;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
 import com.example.finalprojectsecondphase.repository.CustomerRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    Validator validator = validatorFactory.getValidator();
+
+    public void validate(Customer customer) {
+        Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
+        if (violations.isEmpty()) {
+            customerRepository.save(customer);
+            log.info("customer saved");
+        } else {
+            System.out.println("Invalid user data found:");
+            for (ConstraintViolation<Customer> violation : violations) {
+                System.out.println(violation.getMessage());
+            }
+            throw new InvalidInputInformationException("some of inputs are not valid");
+        }
+    }
+    public void saveCustomer(Customer customer) {
+        if (customerRepository.findByUsername(customer.getUsername()).isPresent()) {
+            log.error("duplicate username can not insert");
+            throw new DuplicateInformationException("duplicate username can not insert");
+        } else if (customerRepository.findByEmail(customer.getEmail()).isPresent()) {
+            log.error("duplicate email can not insert");
+            throw new DuplicateInformationException("duplicate email can not insert");
+        } else if (customerRepository.findByPhoneNumber(customer.getPhoneNumber()).isPresent()) {
+            log.error("duplicate phoneNumber can not insert");
+            throw new DuplicateInformationException("duplicate phoneNumber can not insert");
+        } else if (customerRepository.findByPostalCode(customer.getPostalCode()).isPresent()) {
+            log.error("duplicate postalCode can not insert");
+            throw new DuplicateInformationException("duplicate postalCode can not insert");
+        } else {
+            validate(customer);
+        }
+    }
 }
