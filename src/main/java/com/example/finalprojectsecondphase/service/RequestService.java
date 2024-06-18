c4f112ffe6926f54a2fd795f4a9c01af40c84bd8
pc <poyatajhiz59@gmail.com>
2024-06-17 18:12:18 -0800

    add saveRequests RequestService

--- src/main/java/com/example/finalprojectsecondphase/service/RequestService.java
+++ src/main/java/com/example/finalprojectsecondphase/service/RequestService.java
@@ -1,11 +1,23 @@
 package com.example.finalprojectsecondphase.service;
 
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.Request;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
 import com.example.finalprojectsecondphase.repository.RequestRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RequestService {
+
     private final RequestRepository requestRepository;
+    @Transactional
+    public void saveRequests(Request request){
+        requestRepository.save(request);
+    }
+
 }
