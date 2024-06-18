1a82157855fa13210785a81248712306bec81bd5
pc <poyatajhiz59@gmail.com>
2024-06-17 17:09:19 -0800

    add findByExpert OfferRepository

--- src/main/java/com/example/finalprojectsecondphase/repository/OfferRepository.java
+++ src/main/java/com/example/finalprojectsecondphase/repository/OfferRepository.java
@@ -1,7 +1,14 @@
 package com.example.finalprojectsecondphase.repository;
 
+import com.example.finalprojectsecondphase.entity.Expert;
 import com.example.finalprojectsecondphase.entity.Offer;
+import com.example.finalprojectsecondphase.entity.Order;
+import com.example.finalprojectsecondphase.entity.enums.OfferCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OfferRepository extends JpaRepository<Offer,Integer> {
+    List<Offer> findByExpert(Expert expert);
 }
