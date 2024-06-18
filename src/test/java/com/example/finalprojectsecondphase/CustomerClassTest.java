34bc500f712f93b182e39d373cb580a9e00145ac
pc <poyatajhiz59@gmail.com>
2024-06-17 19:20:39 -0800

    add static makeCustomers CustomerClassTest class

--- src/test/java/com/example/finalprojectsecondphase/CustomerClassTest.java
+++ src/test/java/com/example/finalprojectsecondphase/CustomerClassTest.java
@@ -15,4 +15,101 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 class CustomerClassTest {
 
+    @Autowired
+    CustomerService customerService;
+
+    private static Customer correctCustomer;
+    private static Customer correctSecondCustomer;
+    private static Customer duplicateUsernameCustomer;
+    private static Customer duplicteEmailCustomer;
+    private static Customer duplicatePhoneNumberCustomer;
+    private static Customer duplicatePostalCodeCustomer;
+    private static Customer invalidInfoTypeCustomer;
+
+    @BeforeAll
+    public static void makeCustomers() {
+        correctCustomer = Customer.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya123")
+                .password("aaAA12!@")
+                .email("pouya@gmail.com")
+                .phoneNumber("+989124184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1395613373")
+                .build();
+
+        correctSecondCustomer = Customer.builder()
+                .firstname("ali")
+                .lastname("mir")
+                .username("ali123")
+                .password("aaAA12!@")
+                .email("ali@gmail.com")
+                .phoneNumber("+989125694434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1393835763")
+                .build();
+
+        duplicateUsernameCustomer = Customer.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya123")
+                .password("aaAA12!@")
+                .email("pouyaaa@gmail.com")
+                .phoneNumber("+989125184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1417635471")
+                .build();
+
+        duplicteEmailCustomer = Customer.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1234")
+                .password("aaAA12!@")
+                .email("pouya@gmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("4654190118")
+                .build();
+
+        duplicatePhoneNumberCustomer = Customer.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1235")
+                .password("aaAA12!@")
+                .email("pouyaaa@gmail.com")
+                .phoneNumber("+989124184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1665743471")
+                .build();
+
+        duplicatePostalCodeCustomer = Customer.builder()
+                .firstname("pouya")
+                .lastname("mir")
+                .username("pouya1236")
+                .password("aaAA12!@")
+                .email("pouysaa@gmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("1395613373")
+                .build();
+
+        invalidInfoTypeCustomer = Customer.builder()
+                .firstname("pouya@#")
+                .lastname("mir")
+                .username("pouya1236")
+                .password("aaA1!@")
+                .email("pouysaadfagmail.com")
+                .phoneNumber("+989127184434")
+                .city("tehran")
+                .address("ekbatan-A!1")
+                .postalCode("135613373")
+                .build();
+    }
 }
