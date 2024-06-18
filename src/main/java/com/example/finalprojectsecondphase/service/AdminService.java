d35a17cce77dc90e6501972fbd9764facd047ce8
pc <poyatajhiz59@gmail.com>
2024-06-17 17:31:07 -0800

    creat AdminService class

---
+++ src/main/java/com/example/finalprojectsecondphase/service/AdminService.java
@@ -0,0 +1,22 @@
+package com.example.finalprojectsecondphase.service;
+
+import com.example.finalprojectsecondphase.entity.Admin;
+import com.example.finalprojectsecondphase.entity.Expert;
+import com.example.finalprojectsecondphase.entity.Request;
+import com.example.finalprojectsecondphase.entity.SubDuty;
+import com.example.finalprojectsecondphase.entity.enums.ExpertCondition;
+import com.example.finalprojectsecondphase.exception.NotFoundException;
+import com.example.finalprojectsecondphase.repository.AdminRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AdminService {
+
+}
